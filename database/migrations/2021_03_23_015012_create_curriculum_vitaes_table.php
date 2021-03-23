ea28db9d8c0af60ecb1488a4e587f4f85d73279c
Adonai Nangui <adonainangui03@gmail.com>
2021-03-23 02:02:15 +0000

    Created model and migration

---
+++ database/migrations/2021_03_23_015012_create_curriculum_vitaes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCurriculumVitaesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('curriculum_vitaes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->integer('age');
+            $table->string('address');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('speciality');
+            $table->string('level');
+            $table->integer('experience');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('curriculum_vitaes');
+    }
+}
